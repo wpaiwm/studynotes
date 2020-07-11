411efcc0fce411bcd6d61f4c02eb4a415d7df28e
wp <wpaiwm>
2020-07-11 17:45:14 +0800

    200710

--- gitpush.sh
+++ gitpush.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-var1=$[1 + 5]
-echo $var1
-var2=$[$var1*$[5 - 2]]
-echo $var2
-var3=$(expr 1 + 5)
-echo $var3
-var4=$(expr $var1 \* 2)
-echo $var4
-var5=$(echo "scale=4; 3.44 / 5" | bc)
-echo The answer is $var5
+cd /home/wp/mydata/github/studynotes
+git add . >> /home/wp/gitpush.log
+vardate=$(date +%y%m%d)
+git commit -m $vardate >> /home/wp/gitpush.log
+git push -u origin master >> /home/wp/gitpush.log
