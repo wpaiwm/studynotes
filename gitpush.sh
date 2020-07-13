62e7b46bf46a49828fe2fd568c496b001fd00c22
wp <wpaiwm>
2020-07-13 09:50:04 +0800

    200713

--- gitpush.sh
+++
@@ -1,5 +0,0 @@
-cd /home/wp/mydata/github/studynotes
-git add . >> /home/wp/gitpush.log
-vardate=$(date +%y%m%d)
-git commit -m $vardate >> /home/wp/gitpush.log
-git push -u origin master >> /home/wp/gitpush.log
